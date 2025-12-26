dac62de4e4fb63cc6d33a68bb124ba9c1494fb15
vrishank <vrishank.warrier@citiuscloud.com>
2025-12-26 12:32:59 +0530

    initial

---
+++ backend/src/routes/auth.route.js
@@ -0,0 +1,14 @@
+import express from "express"
+
+const router = express.Router()
+
+
+router.get("/signup", (req, res)=> {
+    res.send("Signup endpoint")
+});
+
+router.get("/login", (req, res)=> {
+    res.send("Login endpoint")
+});
+
+export default router
